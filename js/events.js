204071e1a92dacb0626736840789affda2630cd4
Spike Burton <ralphburton3@gmail.com>
2018-10-31 08:05:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input:first-child').on('keydown', function(key) {
+  $('input').on('keydown', function(key) {
     if(key.which == 71) {
       alert('G was pressed')
     }
